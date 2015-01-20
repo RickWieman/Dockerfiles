5f5a9068d6969e569f1deb05fdb63f17e3fb2907
Rick Wieman <git@rickw.nl>
2015-01-20 16:56:14 +0100

    Simplified start script; removed xtail.

--- debian-exim-send/start.sh
+++ debian-exim-send/start.sh
@@ -2,16 +2,10 @@
 
 path=/etc/exim4
 
+# Set environment variables into configuration files
 mkdir -p $path
 echo $PRIMARY_HOST > $path/primary_host
 echo "127.0.0.1 ; ::1 ;" "$ALLOWED_HOSTS" > $path/allowed_hosts
 
-# Make sure spool directory is writable (if a mounted volume)
-chown Debian-exim /var/spool/exim4
-
-# Sort of hack to send logs to stdout
-xtail /var/log/exim4 &
-# Note: Using exec here means Ctrl-C via an attached docker client no
-# longer works - it sends the key strokes to the non-existant shell
-# process, I assume.
-/usr/sbin/exim4 ${*:--bdf -q30m}
+# Start exim
+/usr/sbin/exim4 ${*:--bdf -q30m -v}
